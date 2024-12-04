6b35c7244981a1e193a8867cd1bafb667fe43950
Arjun Majithia <amajithia@blackberry.com>
2024-12-04 12:06:20 -0500

    Initialize day 4

---
+++ day_4/src/main.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int p2() {
+  return 0;
+}
+
+int p1() {
+  std::string filename = "inputs/day1_1.txt";
+  /* std::string filename = "inputs/day1_1.prod"; */
+  std::fstream input_file(filename.c_str(), std::ios::in | std::ios::out);
+  if (!input_file) {
+    std::cerr << "Error opening file\n";
+  }
+
+  std::cout << "Loaded file\n";
+  std::string line;
+  std::vector<std::string> valid_;
+
+  int line_num = 0;
+  if (input_file.is_open()) {
+    while(std::getline(input_file, line))
+    {
+    }
+    input_file.close();
+  }
+  else {
+    std::cout << "file is closed\n";
+  }
+
+  return 0;
+}
+
+int main() {
+#ifdef PART1
+  p1();
+#endif
+#ifndef PART1
+  p2();
+#endif
+}
