a36fe414e41a4ffcfeec785808eaacefe22f37cd
arjun <arj.majithia@gmail.com>
2024-12-08 18:00:51 -0500

    Added day 8

--- day_8/src/main.cpp
+++ day_8/src/main.cpp
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <cctype>
 #include <cstdint>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -10,13 +12,9 @@
 #include "p2_utils.cpp"
 
 int p2() {
-  return 0;
-}
-
-int p1() {
-  /* std::string filename = "inputs/day7_1.txt"; */
+  /* std::string filename = "inputs/day8_1.txt"; */
   /* std::string filename = "inputs/tmp"; */
-  std::string filename = "inputs/day7_1.prod";
+  std::string filename = "inputs/day8_1.prod";
   std::fstream input_file(filename.c_str(), std::ios::in | std::ios::out);
   if (!input_file) {
     std::cerr << "Error opening file\n";
@@ -24,18 +22,142 @@ int p1() {
 
   std::cout << "Loaded file\n";
   std::string line;
+  std::unordered_map<char, std::vector<std::pair<int, int>>> antennas;
+  std::vector<std::vector<char>> map;
 
+  int row = 0;
+  int col = 0;
   if (input_file.is_open()) {
     while(std::getline(input_file, line))
     {
+      col = 0;
+      std::vector<char> ln;
       for (auto c : line) {
+        ln.push_back(c);
+        if (c != '.') {
+          antennas[c].push_back({row, col});
+        }
+        col++;
+        /* std::cout << c; */
       }
+      row++;
+      /* std::cout << std::endl; */
+      map.push_back(ln);
     }
     input_file.close();
   }
   else {
     std::cout << "file is closed\n";
   }
+
+  std::vector<std::pair<int, int>> locations;
+  for (auto a : antennas) {
+    /* std::cout << a.first << ": "; */
+    /* for (auto l : a.second) { */
+    /*   std::cout << "(" << l.first << ", " << l.second << "), "; */
+    /* } */
+    /* std::cout << std::endl; */
+    p2_::get_antennas(a.second, {row, col}, locations);
+  }
+
+  int t = 0;
+  for (auto i = 0; i < map.size(); ++i) {
+    for (auto j = 0; j < map[i].size(); ++j) {
+      if (std::find(locations.begin(), locations.end(), std::make_pair(i, j)) != locations.end()) {
+        /* std::cout << "#"; */
+        t++;
+      }
+      else {
+        if (map[i][j] != '.' && antennas[map[i][j]].size() > 1) {
+          /* std::cout << "#"; */
+          t++;
+        }
+        else {
+          /* std::cout << map[i][j]; */
+        }
+      }
+    }
+    /* std::cout << std::endl; */
+  }
+
+  std::cout << t << std::endl;
+
+  return 0;
+
+}
+
+int p1() {
+  /* std::string filename = "inputs/day8_1.txt"; */
+  /* std::string filename = "inputs/tmp"; */
+  std::string filename = "inputs/day8_1.prod";
+  std::fstream input_file(filename.c_str(), std::ios::in | std::ios::out);
+  if (!input_file) {
+    std::cerr << "Error opening file\n";
+  }
+
+  std::cout << "Loaded file\n";
+  std::string line;
+  std::unordered_map<char, std::vector<std::pair<int, int>>> antennas;
+  std::vector<std::vector<char>> map;
+
+  int row = 0;
+  int col = 0;
+  if (input_file.is_open()) {
+    while(std::getline(input_file, line))
+    {
+      col = 0;
+      std::vector<char> ln;
+      for (auto c : line) {
+        ln.push_back(c);
+        if (c != '.') {
+          antennas[c].push_back({row, col});
+        }
+        col++;
+        std::cout << c;
+      }
+      row++;
+      std::cout << std::endl;
+      map.push_back(ln);
+    }
+    input_file.close();
+  }
+  else {
+    std::cout << "file is closed\n";
+  }
+
+  std::vector<std::pair<int, int>> locations;
+  for (auto a : antennas) {
+    std::cout << a.first << " ";
+  }
+  std::cout << std::endl;
+  for (auto a : antennas) {
+    std::cout << a.first << ": ";
+    /* for (auto l : a.second) { */
+    /*   std::cout << "(" << l.first << ", " << l.second << "), "; */
+    /* } */
+    /* std::cout << std::endl; */
+    p1_::get_antennas(a.second, {row, col}, locations);
+  }
+  int t = 0;
+  for (auto i = 0; i < map.size(); ++i) {
+    for (auto j = 0; j < map[i].size(); ++j) {
+      if (std::find(locations.begin(), locations.end(), std::make_pair(i, j)) != locations.end()) {
+        std::cout << "#";
+        t++;
+      }
+      else {
+        std::cout << map[i][j];
+      }
+    }
+    std::cout << std::endl;
+  }
+  std::cout << t << std::endl;
+  /* for (auto v : map) { */
+  /*   for (auto c : v) { */
+  /*     std::cout << c; */
+  /*   } */
+  /*   std::cout << std::endl; */
+  /* } */
 
   return 0;
 }
--- day_8/src/p1_utils.cpp
+++ day_8/src/p1_utils.cpp
@@ -1,7 +1,61 @@
-#include <cmath>
-#include <cstdint>
+#include <algorithm>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 namespace p1_ {
+
+  bool check_valid_tennas(
+      std::pair<int, int> check_location, 
+      std::pair<int, int> dimensions) {
+
+    if (check_location.first > dimensions.first) {
+      return false;
+    } else if (check_location.first < 0) {
+      return false;
+    }
+    if (check_location.second > dimensions.second) {
+      return false;
+    } else if (check_location.second < 0) {
+      return false;
+    }
+    return true;
+  }
+
+  void get_comp(std::pair<int, int> first, std::pair<int, int> second, std::pair<int, int> dimensions,
+      std::vector<std::pair<int, int>> &locations) {
+    std::pair<int, int> difference = {first.first - second.first, first.second - second.second};
+    std::pair<int, int> new_lhs = {first.first + difference.first, first.second + difference.second};
+    std::pair<int, int> new_rhs = {second.first - difference.first, second.second - difference.second};
+    /* std::cout << new_lhs.first << "," << new_lhs.second << " , "; */
+    /* std::cout << new_rhs.first << "," << new_rhs.second << ": "; */
+    /* std::cout << check_valid_tennas(new_lhs, dimensions) << " , " << check_valid_tennas(new_rhs, dimensions) << " | "; */
+    /* std::cout << std::endl; */
+
+    if (check_valid_tennas(new_lhs, dimensions)) { 
+      if (std::find(locations.begin(), locations.end(), new_lhs) == locations.end()) {
+        locations.push_back(new_lhs); 
+      }
+    }
+    if (check_valid_tennas(new_rhs, dimensions)) { 
+      if (std::find(locations.begin(), locations.end(), new_rhs) == locations.end()) {
+        locations.push_back(new_rhs); 
+      }
+    }
+  }
+
+  int get_antennas(std::vector<std::pair<int, int>> location, std::pair<int, int> dimensions, 
+      std::vector<std::pair<int, int>> &locations) {
+    int out = 0;
+    for (int first = 0; first < location.size(); first++) {
+      for (int second = first + 1; second < location.size(); second++) {
+        if (location[first] == location[second]) { continue; }
+        /* std::cout << location[first].first << "," << location[first].second << " -> "; */
+        /* std::cout << location[second].first << "," << location[second].second << "; "; */
+        get_comp(location[first], location[second], dimensions, locations);
+      }
+    }
+    return out;
+  }
+
 }
