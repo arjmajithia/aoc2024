9f8e4044afb053f365bfb531ab7e7a0f6381a651
arjun <arj.majithia@gmail.com>
2024-12-15 23:16:47 -0500

    Initialize day 15

---
+++ day_15/src/p1_utils.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <set>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+namespace p1_ {
+}
---
+++ day_15/src/p2_utils.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <numeric>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace p2_ {
+}
