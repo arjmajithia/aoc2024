94f92a8c8d006f4c1226c2f42bd22facb498d1e4
Arjun Majithia <amajithia@blackberry.com>
2024-12-18 22:08:19 -0500

    Added day 17. I don't know if I want to do this. I have no idea how the opcodes work and the explanation given does not help

--- day_17/src/main.cpp
+++ day_17/src/main.cpp
@@ -26,16 +26,85 @@ int p1() {
 
   std::cout << "Loaded file\n";
   std::string line;
+  std::vector<int64_t> reg_a, reg_b, reg_c, program;
 
   if (input_file.is_open()) {
     while(std::getline(input_file, line))
     {
+      if (line.find_first_not_of(' ') == std::string::npos) { continue; }
+      std::string pre_c = line.substr(0, line.find(":"));
+      std::string post_c = line.substr(line.find(":") + 2);
+      std::string str_val;
+
+      if (pre_c.find("A") != std::string::npos) { 
+        for (auto c : post_c) {
+          if (isspace(c)) { continue; }
+          if (!isdigit(c)) { 
+            reg_b.push_back(std::stoll(str_val)); 
+            str_val.clear();
+            continue;
+          }
+          str_val += c;
+        }
+        if (!str_val.empty()) { reg_a.push_back(std::stoll(str_val)); str_val.clear(); }
+      }
+      else if (pre_c.find("B") != std::string::npos) { 
+        for (auto c : post_c) {
+          if (isspace(c)) { continue; }
+          if (isspace(c) || !isdigit(c)) { 
+            reg_b.push_back(std::stoll(str_val)); 
+            str_val.clear();
+            continue;
+          }
+          str_val += c;
+        }
+        if (!str_val.empty()) { reg_b.push_back(std::stoll(str_val)); str_val.clear(); }
+      }
+      else if (pre_c.find("C") != std::string::npos) { 
+        for (auto c : post_c) {
+          if (isspace(c) || !isdigit(c)) { 
+            reg_c.push_back(std::stoll(str_val)); 
+            str_val.clear();
+            continue;
+          }
+          str_val += c;
+        }
+        if (!str_val.empty()) { reg_c.push_back(std::stoll(str_val)); str_val.clear(); }
+      }
+      else if (pre_c.find("Program") != std::string::npos) { 
+        for (auto c : post_c) {
+          if (c == ',') { 
+            program.push_back(std::stoll(str_val)); 
+            str_val.clear();
+            continue;
+          }
+          str_val += c;
+        }
+        if (!str_val.empty()) { program.push_back(std::stoll(str_val)); str_val.clear(); }
+      }
     }
     input_file.close();
   }
   else {
     std::cout << "file is closed\n";
   }
+
+  /* std::cout << "Reg A" << std::endl; */
+  /* for (auto v : reg_a) { */
+  /*   std::cout << v << std::endl; */
+  /* } */
+  /* std::cout << "Reg B" << std::endl; */
+  /* for (auto v : reg_b) { */
+  /*   std::cout << v << std::endl; */
+  /* } */
+  /* std::cout << "Reg C" << std::endl; */
+  /* for (auto v : reg_c) { */
+  /*   std::cout << v << std::endl; */
+  /* } */
+  /* std::cout << "Program" << std::endl; */
+  /* for (auto v : program) { */
+  /*   std::cout << v << std::endl; */
+  /* } */
 
   return 0;
 }
